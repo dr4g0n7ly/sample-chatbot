a35c1cd922885b6634e2fab900a71073612b9012
dr4g0n7ly <nishanth.pilli@gmail.com>
2024-06-05 06:53:27 +0530

    Abstract components

--- NextJS/app/chatbot/page.js
+++ NextJS/app/chatbot/page.js
@@ -1,15 +1,98 @@
-import ChatInput from "../components/chat-input";
+'use client'
+import { useState } from 'react';
+import UserMessage from '../components/user-message';
+import ModelMessage from '../components/model-message';
+import ChatInput from '../components/chat-input';
 
 function Header({ title }) {
-    return <h1>{title ? title : 'Default title'}</h1>;
+  return <h1>{title ? title : 'Default title'}</h1>;
 }
 
 export default function Chatbot() {
- 
+  const [userQuery, setUserQuery] = useState(null);
+  const [modelResponse, setModelResponse] = useState(null);
+
+  const updateModelResponse = (token) => {
+    setModelResponse((prevModelResponse) =>
+      prevModelResponse === null ? token : `${prevModelResponse} ${token}`
+    );
+  };
+
+  const handleSendQuery = async (input) => {
+    setUserQuery(input);
+    setModelResponse(null);
+
+    const message = {
+      query: input,
+      createdAt: new Date(),
+      user: {
+        _id: 1,
+        name: "user1",
+      }
+    };
+
+    try {
+      const response = await fetch('http://127.0.0.1:8000/initiate-response', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...message }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to initiate connection');
+      }
+
+      const data = await response.json();
+      const id = data.id;
+      const res = data.response;
+
+      if (id === undefined || id === null) {
+        throw new Error('id is undefined or null');
+      }
+
+      console.log(id, res);  // Output: the actual id and response
+
+      const eventSource = new EventSource(`http://127.0.0.1:8000/get-response/${id}`);
+
+      eventSource.onopen = () => {
+        console.log('EventSource connected');
+      };
+
+      eventSource.addEventListener('tokenStream', function (event) {
+        const res_data = JSON.parse(event.data);
+        if (res_data.status == "completed") {
+          console.log("exited");
+          eventSource.close();
+        }
+        else if (res_data.status == "error") {
+          console.log("error");
+          eventSource.close();
+        } else {
+          const token = res_data.token;
+          console.log('tokenStream', token);
+          updateModelResponse(token);
+        }
+      });
+
+      eventSource.onerror = (error) => {
+        console.error('EventSource failed', error);
+        eventSource.close();
+      };
+
+    } catch (error) {
+      console.error('Error:', error);
+      setModelResponse('Internal Server Error')
+    }
+  };
+
   return (
     <div>
       <Header title="Chatbot." />
-      <ChatInput/>
+      {userQuery && <UserMessage text={userQuery} />}
+      {modelResponse && <ModelMessage text={modelResponse} />}
+      <ChatInput onSendQuery={handleSendQuery} />
     </div>
   );
 }
--- NextJS/app/components/chat-input.js
+++ NextJS/app/components/chat-input.js
@@ -1,135 +1,39 @@
-'use client'
-import styles from './chat-input.module.css'
-import { useState } from "react"
+import styles from './chat-input.module.css';
+import { useState } from "react";
 
-const ChatInput = () => {
-  
-    const [prompt, setPrompt] = useState('')
-    const [userQuery, setUserQuery] = useState(null)
-    const [modelResponse, setModelResponse] = useState(null)
+const ChatInput = ({ onSendQuery }) => {
+  const [prompt, setPrompt] = useState('');
 
-    const updateModelResponse = (token) => {
-      setModelResponse((prevModelResponse) =>
-        prevModelResponse === null ? token : `${prevModelResponse} ${token}`
-      );
-    };
-  
-    const handleSendQuery = async (e) => {
-      e.preventDefault()
-      if (!prompt) {
-        console.log("prompt cannot be empty")
-        return
-      }
-  
-      const input = prompt.trim()
-      setUserQuery(input)
-      setPrompt("")
-      setModelResponse(null)
-  
-      const message = {
-        query: input,
-        createdAt: new Date(),
-        user: {
-          _id: 1,
-          name: "user1", 
-        }
-      }
-
-      try {
-        const response = await fetch('http://127.0.0.1:8000/initiate-response', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ ...message }),
-        });
-
-        if (!response.ok) {
-          throw new Error('Failed to initiate connection');
-        }
-
-        const data = await response.json();
-        const id = data.id
-        const res = data.response
-
-        if (id === undefined || id === null) {
-          throw new Error('id is undefined or null');
-        }
-
-        console.log(id, res);  // Output: the actual id and response
-
-        const eventSource = new EventSource(`http://127.0.0.1:8000/get-response/${id}`);
-
-        eventSource.onopen = () => {
-            console.log('EventSource connected');
-        };
-
-        eventSource.addEventListener('tokenStream', function (event) {
-            const res_data = JSON.parse(event.data);
-            if (res_data.status == "completed") {
-                console.log("exited");
-                eventSource.close();
-            }
-            else if (res_data.status == "error") {
-              console.log("error");
-              setModelResponse("Internal Server Error")
-              eventSource.close();
-            } else {
-                const token = res_data.token
-                console.log('tokenStream', token);
-                updateModelResponse(token)
-            }
-        });
-
-        eventSource.onerror = (error) => {
-            console.error('EventSource failed', error);
-            eventSource.close();
-        };
-
-      } catch (error) {
-        setModelResponse("Internal Server Error")
-        if (error.message.includes('Failed to fetch')) {
-          console.error('CORS error or Network issue:', error);
-        } else {
-          console.error('Error:', error);
-        }
-      }
+  const handleSendQuery = (e) => {
+    e.preventDefault();
+    if (prompt.trim() !== '') {
+      onSendQuery(prompt.trim());
+      setPrompt('');
     }
-  
-    return (
-      <div>
-        {userQuery && (
-          <div className={styles.userText}>
-            <p className={styles.innerUserText}>User: {userQuery}</p>
-          </div>
-        )}
-        {modelResponse && (
-          <div className={styles.modelText}>
-            <p>AI: {modelResponse}</p>
-          </div>
-        )}
-        <div className={styles.chatInput}>
-          <form onSubmit={handleSendQuery} className={styles.chatInputForm} >
-            <input
-              value={prompt}
-              onChange={e => setPrompt(e.target.value)}
-              type="text" 
-              placeholder="Type your message here..." 
-              className={styles.chatInputInput}
-            />
-            <button type="submit" className={styles.chatInputButton} disabled={prompt===''}>
-              <img
-                src='/assets/send-icon.svg'
-                alt='send-icon'
-                width={22}
-                height={22}
-                className={styles.chatInputImg}
-              />
-            </button>
-          </form>
-        </div>
-      </div>
-    )
-  }
-  
-  export default ChatInput
+  };
+
+  return (
+    <div className={styles.chatInput}>
+      <form onSubmit={handleSendQuery} className={styles.chatInputForm}>
+        <input
+          value={prompt}
+          onChange={e => setPrompt(e.target.value)}
+          type="text"
+          placeholder="Type your message here..."
+          className={styles.chatInputInput}
+        />
+        <button type="submit" className={styles.chatInputButton} disabled={prompt === ''}>
+          <img
+            src='/assets/send-icon.svg'
+            alt='send-icon'
+            width={22}
+            height={22}
+            className={styles.chatInputImg}
+          />
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatInput;
